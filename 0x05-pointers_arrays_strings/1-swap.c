3769ec90df6183004ba073418a848605f75b1b80
eedriz99 <akinsolaidris1999@gmail.com>
2023-02-28 03:15:05 -0800

    README file added plus some changes to the swap task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -13,9 +13,9 @@ void swap_int(int *a, int *b)
 {
 	int placeholder_variable;
 
-	placeholder_variable = &a;
-	*a = &b;
-	*b = &placeholder_variable;
+	placeholder_variable = *a;
+	*a = *b;
+	*b = placeholder_variable;
 
 	return (0);
 }
